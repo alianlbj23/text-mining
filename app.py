739ba03bf780d25085c00caad0a89ed4516908e3
alianlbj23 <alianlbj23@gmail.com>
2022-03-02 08:17:19 +0800

    s

--- app.py
+++ app.py
@@ -758,4 +758,4 @@ def download_lda(n_clicks, list_of_contents, list_of_names, list_of_dates, topic
 #                                     )
 
 if __name__ == '__main__':
-    app.run_server(debug=TRUE)
+    app.run_server(debug=False)
